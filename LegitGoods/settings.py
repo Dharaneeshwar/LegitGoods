9f05c0f3cd308ba1470954fda03480e3af601003
Dharaneeshwar <daranip@gmail.com>
2021-03-14 15:05:18 +0530

    😅 Debug in production

--- LegitGoods/settings.py
+++ LegitGoods/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'khgrw*#efsnxb$ku(!up@=0g4q&zr8xzb35=c)iqg8u7+#0pv+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','legitgoods.herokuapp.com']
 
